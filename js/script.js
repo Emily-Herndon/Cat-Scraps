d695fbc2fb181cfc3e041c8fe9af273525cbf57d
Emily-Herndon <emilyvherndon@gmail.com>
2022-05-08 11:26:17 -0700

    food now renders at different X values

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ console.log('Stinky play time!')
 /*DOM SELECTORS */
 const canvas = document.querySelector('#canvas')
 /*CANVAS SETUP/ GAME STATE */
+
 const ctx = canvas.getContext('2d')
 canvas.width = 1000
 const canvW = canvas.width
 canvas.height = 400
 const canvH = canvas.height
 console.log(canvW, canvH)
+function generateX (){
+    const randX = Math.floor(Math.random()*canvas.width)
+    return randX
+}
 const randX = Math.floor(Math.random()*canvas.width)
 
 const gameLoopInterval = setInterval(gameLoop, 60)
@@ -34,16 +39,16 @@ class Character {
 const cat = new Character(500, 320, 'grey', 50, 75, 'player')
 // cat.render()
 //falling chicken
-const chicken = new Character(randX, 0, 'rgb(121, 65, 71', 30, 30, 'food')
+const chicken = new Character(generateX(), 0, 'rgb(121, 65, 71', 30, 30, 'food')
 // chicken.render()
 //falling turkey
-const turkey = new Character(randX, 0, 'rgb(197, 114, 86', 30, 30, 'food')
+const turkey = new Character(generateX(), 0, 'rgb(197, 114, 86', 30, 30, 'food')
 // turkey.render()
 //falling fish
-const fish = new Character(randX, 0, 'rgb(168, 211, 254', 30, 30, 'food')
+const fish = new Character(generateX(), 0, 'rgb(168, 211, 254', 30, 30, 'food')
 // fish.render()
 //falling cheese
-const cheese = new Character(randX, 0, 'rgb(250, 192, 48', 30, 30, 'cheese')
+const cheese = new Character(generateX(), 0, 'rgb(250, 192, 48', 30, 30, 'cheese')
 // cheese.render()
 
 /*GAME FUNCTIONS */
@@ -54,7 +59,7 @@ function movementHandler(e) {
         switch(e.key) {
             case('ArrowLeft'):
             case('a'):
-                cat.x = 0
+                cat.x = 1
             break
             case('ArrowRight'):
             case('d'):
@@ -104,12 +109,16 @@ function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     //render the game objects
     cat.render()
+    draw()
+
+}
+
+function draw() {
     chicken.render()
     turkey.render()
     fish.render()
     cheese.render()
 }
-
 
 /* EVENT LISTENERS */
 canvas.addEventListener('click', e => {
